e005fa825524f2b23af839582af2b6002ee4a729
777Jose <espionjose132@gmail.com>
2024-12-07 13:40:14 -0400

    Primer commit DB

---
+++ blueprints/login/__init__.py
@@ -0,0 +1,91 @@
+from flask import Blueprint, render_template, redirect, url_for, session, request
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
+from functools import wraps
+
+login_bp = Blueprint("login",__name__, template_folder="templates")
+
+
+def login_required(f):
+    @wraps(f)
+    def decorate_fuction(*args, **kwargs):
+        if 'user_id' not in session:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorate_fuction
+
+@login_bp.route("/")
+def index():
+    return render_template("index.html")
+
+@login_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        nombre = request.form.get('nombre')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Validaciones básicas
+        if not nombre or not email or not password:
+            return "Todos los campos son obligatorios."
+
+        # if rol not in ['user']:
+        #     return "El rol debe ser 'admin' o 'user'.", 400
+
+        password_encriptado = generate_password_hash(password)
+
+        try:
+            with sqlite3.connect('biblioteca.db') as conn:
+                cursor = conn.cursor()
+
+                cursor.execute('SELECT * FROM usuarios WHERE email = ?', (email,))
+                if cursor.fetchone():
+                    return "El email ya está registrado. Usa otro email."
+
+                cursor.execute(
+                    'INSERT INTO usuarios (nombre, email, password, rol) VALUES (?, ?, ?, "user")',
+                    (nombre, email, password_encriptado)
+                )
+                conn.commit()
+
+            return redirect(url_for('login.login'))
+        except sqlite3.Error as e:
+            return f"Error en la base de datos: {e}"
+    return render_template('register.html')
+
+
+@login_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if not email or not password:
+            return "El email y la contraseña son obligatorios.", 400
+
+        try:
+            with sqlite3.connect('biblioteca.db') as conn:
+                conn.row_factory = sqlite3.Row 
+                cursor = conn.cursor()
+                cursor.execute('SELECT * FROM usuarios WHERE email = ?', (email,))
+                usuario = cursor.fetchone()
+
+                if usuario and check_password_hash(usuario['password'], password):
+                    session['user_id'] = usuario['id_usuario'] 
+                    return redirect(url_for('login.index'))
+
+                return "Credenciales incorrectas.", 401
+        except sqlite3.Error as e:
+            return f"Error en la base de datos: {e}", 500
+
+    return render_template('login.html')
+
+
+@login_bp.route('/logout')
+def logout():
+    session.pop('user_id', None)
+
+    return redirect('/')
+
+@login_bp.route('/admin/dashboard')
+def dashboard():
+    return render_template('/admin/dashboard.html')
---
+++ main.py
@@ -0,0 +1,80 @@
+from flask import Flask
+import sqlite3
+from blueprints.login import login_bp
+
+app = Flask(__name__)
+app.secret_key = 'miclavesecreta'
+
+def init_db():
+    conn = sqlite3.connect('biblioteca.db')
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS usuarios (
+        id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        rol TEXT CHECK(rol IN ('admin', 'user')) NOT NULL,
+        fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        """)
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS autores (
+        id_autor INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        apellido TEXT NOT NULL,
+        biografia TEXT
+        )
+        """
+    )
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS categorias (
+        id_categoria INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT UNIQUE NOT NULL,
+        descripcion TEXT
+        )
+        """
+    )
+        
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS libros (
+        id_libro INTEGER PRIMARY KEY AUTOINCREMENT,
+        titulo TEXT NOT NULL,
+        id_autor INTEGER NOT NULL,
+        id_categoria INTEGER NOT NULL,
+        descripcion TEXT,
+        archivo TEXT NOT NULL,
+        fecha_publicacion DATE,
+        fecha_agregado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (id_autor) REFERENCES Autores(id_autor),
+        FOREIGN KEY (id_categoria) REFERENCES Categorias(id_categoria)
+        )
+        """
+    )
+        
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS historial (
+        id_historial INTEGER PRIMARY KEY AUTOINCREMENT,
+        id_usuario INTEGER NOT NULL,
+        id_libro INTEGER NOT NULL,
+        fecha_descarga TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
+        FOREIGN KEY (id_libro) REFERENCES Libros(id_libro)
+        )
+        """
+    )
+    conn.commit()
+    conn.close()
+
+init_db()
+
+app.register_blueprint(login_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
